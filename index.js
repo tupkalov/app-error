ec3192cfe099cfba36fe0c6a57c5ff5bddd6c5e9
tupkalov <tupkalov@gmail.com>
2017-04-16 10:56:36 +0300

    error assign options

--- index.js
+++ index.js
@@ -1,125 +1,101 @@
 const InternalError = 'InternalError';
-const defaultErrorLangMap = {
-	App : { InternalError }
-}
 
 const parseFromString = message => {
 	let indexSeparator = message.indexOf('::', 2);
 	return [message.substring(2, indexSeparator), message.substring(indexSeparator+2)]
 }
 
-module.exports = function(errorLangMap = defaultErrorLangMap){
 
-	class AppError extends Error {
+class AppError extends Error {
 
-		constructor(first, second, third) {
-			var wrappedFrom, wrappedMessage;
+	constructor(first, second, third) {
+		var wrappedFrom, wrappedMessage;
 
-			var from, message, options, wrappedMessageMap;
+		var from, message, options, wrappedMessageMap;
 
-			// message == null
-			if(!first)
-				first = InternalError;
+		// message == null
+		if(!first)
+			first = InternalError;
 
-			// message[, options]
-			if(typeof first === "string"){
-				from = null;
-				message = first;
+		// message[, options]
+		if(typeof first === "string"){
+			from = null;
+			message = first;
 
-				if(message.startsWith('::'))
-					wrappedMessageMap = parseFromString(message);
+			if(message.startsWith('::'))
+				wrappedMessageMap = parseFromString(message);
 
-				if(typeof second === "object")
-					options = second;
-				else
-					options = {};
-			}else
-
-			// error[, message][, options]
-			if(!(first instanceof AppError))
-				throw new Error('Unexpected typeof first argument');
-
-			else{
-				from = first;
-
-				// error, message[, options]
-				if(typeof second === "string"){
-					message = second;
-					options = third || {};
-
-				// error[, options]
-				}else{
-					if(from.message.startsWith('::')){
-						message = from.message;
-						wrappedMessageMap = parseFromString(message)
-					}else{
-						message = `::${from.ns}::${from.message}`;
-						wrappedMessageMap = [from.ns, from.message];
-					}
-
-					options = second || {};
-				}
-			}
-
-			super(message);
-
-			var { from, info } = options;
-
-			if(wrappedMessageMap){
-				this.wrappedFrom = wrappedMessageMap[0];
-				this.wrappedMessage = wrappedMessageMap[1];
-			}
-
-			this.from 		= from;
-			this.info 		= info;
-
-
-			if (typeof Error.captureStackTrace === 'function')
-				Error.captureStackTrace(this, this.constructor);
-
-			// client implimentation
+			if(typeof second === "object")
+				options = second;
 			else
-				this.stack = (new Error(message)).stack; 
-		}
+				options = {};
+		}else
 
-		toJSON (){
-			return this.message;
-		}
+		// error[, message][, options]
+		if(!(first instanceof AppError))
+			throw new Error('Unexpected typeof first argument');
 
-		toString () {
-			return this.message;
-		}
+		else{
+			from = first;
 
-		toLocaleString () {
-			var ns = this.wrappedFrom || this.ns,
-				message = this.wrappedMessage || this.message;
+			// error, message[, options]
+			if(typeof second === "string"){
+				message = second;
+				options = third || {};
 
-			let errorNS = errorLangMap[ns];
-			if (typeof errorNS !== "object")
-				throw new Error(`No map for "${ns}" namespace`);
-
-			var result = errorNS[message];
-
-			if(!result && message === InternalError)
-				result = InternalError;
-
-			if(typeof result === "function")
-				result = result(this);
-
-			return result;
-		}
-
-		static extend(ns) {
-
-			return class extends this {
-
-				get ns () {
-					return ns
+			// error[, options]
+			}else{
+				if(from.message.startsWith('::')){
+					message = from.message;
+					wrappedMessageMap = parseFromString(message)
+				}else{
+					message = `::${from.ns}::${from.message}`;
+					wrappedMessageMap = [from.ns, from.message];
 				}
-			}
 
+				options = second || {};
+			}
 		}
+
+		super(message);
+
+		Object.assign(this, options);
+
+		if(wrappedMessageMap){
+			this.wrappedFrom = wrappedMessageMap[0];
+			this.wrappedMessage = wrappedMessageMap[1];
+		}
+
+		this.from 		= from;
+		this.info 		= info;
+
+
+		if (typeof Error.captureStackTrace === 'function')
+			Error.captureStackTrace(this, this.constructor);
+
+		// client implimentation
+		else
+			this.stack = (new Error(message)).stack; 
 	}
 
-	return AppError.extend('App');
-};
+	toJSON (){
+		return this.message;
+	}
+
+	toString () {
+		return this.message;
+	}
+
+	static extend(ns) {
+
+		return class extends this {
+
+			get ns () {
+				return ns
+			}
+		}
+
+	}
+}
+
+module.exports = AppError.extend('App');
--- test/test.js
+++ test/test.js
@@ -1,22 +1,10 @@
 const assert = require('assert');
 
 describe('testing AppError', function(){
-	const errorLangMap = {
-		App : {
-			'WithTemplateError' : (error) => `TemplateError with json: ${JSON.stringify(Object.assign(error.info))}`
-		},
-		Extended : {
-			'Test:Error' : 'This is test error!',
-			'InternalError' : 'Anything internal error'
-		},
-		Wrapped : {
-			'Any:Error' : ({ info : { testString }, ns }) => `Wrapped Error! In ${ns}Error. testString: ${testString}`
-		}
-	};
 
 
 	describe('ExtendedError without langMap', function(){
-		const AppError = require('..')();
+		const AppError = require('..');
 		const ExtendedError = AppError.extend('Extended');
 
 		var error = new ExtendedError('Test:Error', { info : { testValue : 'xxx'} });
@@ -44,49 +32,10 @@ describe('testing AppError', function(){
 			}, error, `Not deep equal example: ${JSON.stringify(Object.assign({}, error))}`)	
 		});
 
-		it('toLocaleString - Catch no ns map error ', function(){
-			try{
-				error.toLocaleString()
-			}catch(e){
-				assert.equal(e.message, `No map for "${error.ns}" namespace`)
-			}
-		});
-
-	});
-
-	describe('ExtendedError with langMap', function(){
-		const AppError = require('..')(errorLangMap);
-		const ExtendedError = AppError.extend('Extended');
-
-
-
-		it('toLocaleString - default for InternalError', function(){
-			var error = new AppError;
-			assert.equal(error.toLocaleString(), 'InternalError');
-		})
-
-		it('toLocaleString - String', function(){
-			var error = new ExtendedError('Test:Error');
-			assert.equal(error.toLocaleString(), errorLangMap.Extended['Test:Error']);
-		})
-
-		it('toLocaleString - String for InternalError', function(){
-			var error = new ExtendedError;
-			assert.equal(error.toLocaleString(), 'Anything internal error');
-		})
-
-		it('toLocaleString - Template', function(){
-			var error = new AppError('WithTemplateError', {info : {testValue : 'XXX'}});
-			assert.equal(
-				error.toLocaleString(), 
-				'TemplateError with json: {"testValue":"XXX"}'
-			)
-		})
-
 	});
 
 	describe('Extended with message nested error', function(){
-		const AppError = require('..')(errorLangMap);
+		const AppError = require('..');
 		const ExtendedError = AppError.extend('Extended');
 		const WrappedError = AppError.extend('Wrapped');
 
@@ -96,25 +45,6 @@ describe('testing AppError', function(){
 			var extended = new ExtendedError(wrapped);
 
 			assert.equal(extended.message, '::Wrapped::Any:Error');
-		})
-
-		it('Wrapped Error - from error', function(){
-			var wrapped = new WrappedError('Any:Error');
-			var extended = new ExtendedError(wrapped, {info : { testString : 'Hey!'}});
-
-			assert.equal(extended.toLocaleString(), `Wrapped Error! In ExtendedError. testString: Hey!`);
-
-		})
-
-		it('WrappedError - from string', function(){
-			var error = new WrappedError('::Extended::Test:Error');
-			assert.equal(error.toLocaleString(), 'This is test error!')
-		});
-
-		it('WrappedError - from error and message', function(){
-			var wrapped = new WrappedError('Any:Error');
-			var extended = new ExtendedError(wrapped, 'Test:Error');
-			assert.equal(extended.toLocaleString(), 'This is test error!');
 		})
 	})
 })
